b9f3c825e8bc601ce88411504db520c5f3783aa4
lizekang <zekangli97@gamil.com>
2018-09-19 15:06:45 +0800

    fix bugs

--- stargan.py
+++ stargan.py
@@ -34,7 +34,7 @@ parser.add_argument('--train_step', type=int, default=100000, help='epoch to sta
 parser.add_argument('--test_step', type=int, default=10, help='epoch to start training from')
 parser.add_argument('--n_train_step', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--data_path', type=str, default="3dletter_list.txt", help='number of epochs of training')
-parser.add_argument('--batch_size', type=int, default=32, help='size of the batches')
+parser.add_argument('--batch_size', type=int, default=16, help='size of the batches')
 parser.add_argument('--retrain', type=bool, default=False, help='if retrain')
 parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
 parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
@@ -43,7 +43,7 @@ parser.add_argument('--decay_epoch', type=int, default=100, help='epoch from whi
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--img_height', type=int, default=128, help='size of image height')
 parser.add_argument('--img_width', type=int, default=128, help='size of image width')
-parser.add_argument('--channels', type=int, default=9, help='number of image channels')
+parser.add_argument('--channels', type=int, default=3, help='number of image channels')
 parser.add_argument('--c_dims', type=int, default=15, help='number of views')
 parser.add_argument('--sample_interval', type=int, default=100,
                     help='interval between sampling of images from generators')
